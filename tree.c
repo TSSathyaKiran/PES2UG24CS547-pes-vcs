a3cd378013f47d575b4f8b142ca5b08c14ede3b1
TSSathyaKiran <tssathya26@gmail.com>
2026-04-19 01:20:38 +0530

    added forward declaration

--- tree.c
+++ tree.c
@@ -18,6 +18,9 @@
 #include "index.h"
 // ─── Mode Constants ─────────────────────────────────────────────────────────
 
+int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
+int index_load(Index *index);
+
 #define MODE_FILE      0100644
 #define MODE_EXEC      0100755
 #define MODE_DIR       0040000
